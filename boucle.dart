1d668df8f238bd626b21d8f768e9e8a08987c190
AshDest <ashuzamaheshe4@gmail.com>
2023-07-03 17:36:35 +0200

    boucle notion

--- boucle.dart
+++ boucle.dart
@@ -1,5 +1,39 @@
 main() {
-  for (int i = 0; i < 10; i++) {
-    print("Salut les codeurs numero $i");
+  List<String> courses = ["John", "Doe", "Jane", "Doe", "Foo", "Bar", "Baz"];
+
+  Map<String, int> ages = {"John": 25, "Doe": 30, "Jane": 20};
+
+//for Range
+  for (int i = 0; i < courses.length; i++) {
+    print(courses[i]);
   }
+  print("----");
+  //for Element
+  for (String course in courses) {
+    print(course);
+  }
+  print("======== ------ ========");
+  for (String key in ages.keys) {
+    print(key);
+  }
+  //For Each
+  courses.forEach((course) => print(course));
+  print("======== ------ ========");
+  courses.forEach((course) {
+    print("Mes courses: $course");
+  });
+
+  //While
+  int age = 0;
+  while (age <= 25) {
+    print("j'ai $age et je reste chez mes parents");
+    age++;
+  }
+
+  //Do While
+  int age2 = 0;
+  do {
+    print("j'ai $age2 et je reste chez mes parents");
+    age2++;
+  } while (age2 <= 25);
 }
